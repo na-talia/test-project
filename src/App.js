5292ecd955bec6cba22b0c2bebbf3ede84d4211b
na-talia <ntsh_89@ukr.net>
2023-08-05 22:33:07 +0200

    added input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function App() {
   const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState("Text in input");
 
   function increment() {
     setLikes(likes + 1);
@@ -14,6 +15,12 @@ function App() {
   return (
     <div className="App">
       <h1>{likes}</h1>
+      <h2>{value}</h2>
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
