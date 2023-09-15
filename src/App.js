74056e0151470a944d41f0b6af24f856b71cedbb
na-talia <ntsh_89@ukr.net>
2023-09-16 00:19:26 +0200

    added Links to /about and /posts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./styles/App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
 
 function App() {
   return (
     <BrowserRouter>
+      <div className="navbar">
+        <div className="navbar__links">
+          <Link to="/about">About us</Link>
+          <Link to="/posts">Posts</Link>
+        </div>
+      </div>
       <Routes>
         <Route path="/about" element={<About />}></Route>
         <Route path="/posts" element={<Posts />}></Route>
