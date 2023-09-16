8b90326d7b5ce7794b2313236cb6e0babc27ad9d
na-talia <ntsh_89@ukr.net>
2023-09-16 23:47:07 +0200

    moved Navbar into a new component. Added path to /*

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from "react";
 import "./styles/App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
+import Navbar from "./components/UI/Navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="navbar">
-        <div className="navbar__links">
-          <Link to="/about">About us</Link>
-          <Link to="/posts">Posts</Link>
-        </div>
-      </div>
+      <Navbar />
       <Routes>
         <Route path="/about" element={<About />}></Route>
         <Route path="/posts" element={<Posts />}></Route>
+        <Route path="/*" element={<Posts />}></Route>
       </Routes>
     </BrowserRouter>
   );
