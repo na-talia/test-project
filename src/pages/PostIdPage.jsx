16fd523832c66c31d615eb3ca4e19b571b264c29
na-talia <ntsh_89@ukr.net>
2023-09-22 00:27:31 +0200

    added comments and emails by id

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -7,13 +7,20 @@ import Loader from "../components/UI/Loader/Loader";
 const PostIdPage = () => {
   const params = useParams();
   const [post, setPost] = useState({});
+  const [comments, setComments] = useState([]);
   const [fetchPostById, isLoading, error] = useFetching(async (id) => {
     const response = await PostService.getById(id);
     setPost(response.data);
   });
 
+  const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+    const response = await PostService.getCommentByPostId(id);
+    setComments(response.data);
+  });
+
   useEffect(() => {
     fetchPostById(params.id);
+    fetchComments(params.id);
   }, []);
 
   return (
@@ -24,6 +31,19 @@ const PostIdPage = () => {
       ) : (
         <div>
           {post.id}. {post.title}
+        </div>
+      )}
+      <h2>Comments</h2>
+      {isComLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          {comments.map((comm) => (
+            <div style={{ marginTop: 15 }}>
+              <h5>{comm.email}</h5>
+              <div>{comm.body}</div>
+            </div>
+          ))}
         </div>
       )}
     </div>
