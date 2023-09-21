f30aed1c79263b7ee9a7f7e9f3c0aae2401c21b9
na-talia <ntsh_89@ukr.net>
2023-09-22 00:48:44 +0200

    removed some imports inside AppRouter

--- src/components/UI/AppRouter.jsx
+++ src/components/UI/AppRouter.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import About from "../../pages/About";
-import Posts from "../../pages/Posts";
-import Error from "../../pages/Error";
-import PostIdPage from "../../pages/PostIdPage";
 import { routes } from "../../router";
 
 const AppRouter = () => {
