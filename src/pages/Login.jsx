a37171b50fe8c3026920496a358bffe987e62024
na-talia <ntsh_89@ukr.net>
2023-09-27 01:25:08 +0200

    added useContest for using login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import MyInput from "../components/UI/input/MyInput";
 import MyButton from "../components/UI/button/MyButton";
+import { AuthContext } from "../context";
 
 const Login = () => {
+  const { isAuth, setIsAuth } = useContext(AuthContext);
+  const login = (event) => {
+    event.preventDefault();
+    setIsAuth(true);
+  };
   return (
     <div>
       <h1>Login Page</h1>
-      <form>
+      <form onSubmit={login}>
         <MyInput type="text" placeholder="Enter Login" />
         <MyInput type="text" placeholder="Enter Password" />
         <MyButton>Log In</MyButton>
